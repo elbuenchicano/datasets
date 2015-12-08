d7d6b29b72bdd7db7d96ac6104a805491af74c8d
rensso <soulchicano@gmail.com>
2015-12-08 00:22:19 -0200

    - weak without tags

--- DataSetDot/DataSetDot.py
+++ DataSetDot/DataSetDot.py
@@ -1,7 +1,8 @@
 #dot language package : pip install graphviz
 import graphviz as gv
 import glob, os
-import functools
+import functools 
+import fileinput
 
 def add_nodes(graph, nodes):
     for n in nodes:
@@ -57,6 +58,13 @@ styles = {
 }
 
 ################################################################################
+def readTagsFromFile(filename):
+    for line in fileinput.input(filename):
+        pos = line.find("tags:")
+        if pos > -1:
+            tags = line.split("tags:",1)
+            return tags
+        
 ###############################################################################
 #get the last name
 def ridofTwoPoitns_Back(str):
@@ -75,18 +83,29 @@ def getLastName(root):
 #label builder makes the table for the node label
 def builderLabel(root, full):
     str = '< <TABLE bgcolor="beige" cellspacing="0" border="0" cellborder="0">'
-    str = str + '<TR><TD href="' + full + '">'
+    str = str + '<TR><TD href="' + full.replace("\\","/") + '">'
     str = str + getLastName(root)
     str = str + '</TD></TR> </TABLE>>'
     return str
+
+###############################################################################
+#label builder makes the table for the node label
+def builderLabelInfo(root, full, info):
+    str = '< <TABLE bgcolor="beige" cellspacing="0" border="0" cellborder="0">'
+    str = str + '<TR><TD href="' + full.replace("\\","/") + '">'
+    str = str + getLastName(root)   + '</TD></TR>' 
+    str = str + '<TD><TR>' + info   + '</TD></TR> </TABLE>>'
+    return str
+
 
 ###############################################################################
 #directory: source directory
 def buildGraph(directory, outDirectory):
     #svg html format
     #creating the graph base
-    graph = functools.partial(gv.Graph, format='svg')
-    edges = []
+    graph   = functools.partial(gv.Graph, format='svg')
+    edges   = []
+    
     #appending the root
     cleanD  = ridofTwoPoitns_Back(directory)
     nodes   = [(cleanD, {'label': builderLabel(directory, directory)})]
@@ -94,84 +113,47 @@ def buildGraph(directory, outDirectory):
     #walking trouhgt directory
     for root, dirs, files in os.walk(dir):
         print(root)
-        for dire in dirs:
-            full    = os.path.join(root, dire)
-            lbl     = builderLabel(dire, full)
-            cleanD  = ridofTwoPoitns_Back(full)
-            cleanR  = ridofTwoPoitns_Back(root)
-            nodes.append(
-                (cleanD, {'label': lbl})
-            )
-            edges.append(
-                (cleanR, cleanD)
-            )
+        hasinfo = False
+        finfo   = ""
 
+        #looking for info file
         for file in files:
             if file.endswith(".info"):
-            #print(os.path.join(root, file))
-                print(file)
+                finfo    = print(os.path.join(root, file))
+                hasinfo = True
+
+        #only directories with info
+        if hasinfo == True:
+            #tags = readTagsFromFile(finfo)
+            for dire in dirs:
+                full    = os.path.join(root, dire)
+                lbl     = builderLabel(dire, full)
+                cleanD  = ridofTwoPoitns_Back(full)
+                cleanR  = ridofTwoPoitns_Back(root)
+                nodes.append(
+                    (cleanD, {'label': lbl})
+                )
+                edges.append(
+                    (cleanR, cleanD)
+                )
+
+    #creating the graph    
     out = add_edges(
         add_nodes(graph(),nodes),
         edges
     ) 
+    #applying the style
     out = apply_styles(out, styles)
     out.render(outDirectory)
     
 #end buildgraph
 ################################################################################
-
-#g1 = gv.Graph(format='svg')
-#g1.node('A')
-#g1.node('B')
-#g1.edge('A', 'B')
-#print(g1.source)
-#filename = g1.render(filename='s:/Documents/g1')
-
-
-#graph = functools.partial(gv.Graph, format='svg')
-#digraph = functools.partial(gv.Digraph, format='svg')
-
-#stri = "d:\graderia"
-
-#nodes = [ 'B', ('C', {})]
-#lbl = builderLabel(stri)
-##nodes.append(('A', {'label': '<<TABLE bgcolor="beige" cellspacing="0" border="0" cellborder="0"> <TR><TD href="http:www.google.com">OUR BUSINESS SERVICES</TD></TR> </TABLE>>'}))
-#nodes.append(('A', {'label': lbl}))
-
-#edges = [
-#    ('A', 'B'),
-#    ('B', 'C'),
-#    (('A', 'C'), {}),
-#]
-
-
-#g6 = add_edges(
-#    add_nodes(graph(), [
-#        ('A', {'label': '<<TABLE bgcolor="beige" cellspacing="0" border="0" cellborder="0"> <TR><TD href="http:www.google.com">OUR BUSINESS SERVICES</TD></TR> </TABLE>>'}),
-#        ('B', {'label': 'Node B'}),
-#        'C'
-#    ]),
-#    [
-#        ('A', 'B'),
-#        ('A', 'C'),
-#        ('B', 'C')
-#    ]
-#)
-
-#g6 = add_edges(
-#    add_nodes(graph(),nodes),
-#    edges
-#)
-
-#g6 = apply_styles(g6, styles)
-#g6.render('s:/Documents/g1')
-
 ######################################################################################
 ######################################################################################
 ######################################################################################
 if __name__ == "__main__":
-    dir     = "z:\\"
-    outdir  = "s:/Documents/g1"
+    dir     = "z:\\DATASETS"
+    outdir  = "s:/Documents/g2"
     buildGraph(dir, outdir)
     
     
